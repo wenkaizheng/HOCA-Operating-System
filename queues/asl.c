737eabe1dde82205d0e143136b27a51ab3053885
wenkai zheng <31551859+wenkaizheng@users.noreply.github.com>
2022-01-23 22:41:40 -0500

    queue: add source code for asl and procq

---
+++ queues/asl.c
@@ -0,0 +1,182 @@
+#include "../h/const.h"
+#include "../h/types.h"
+#include "../h/procq.e"
+#include "../h/asl.h"
+#include "../h/asl.e"
+
+semd_t semd_table[MAXPROC];
+semd_t* cur_sem;
+semd_t* free_head;
+
+void setSem(int *semAdd, proc_t *p){
+    for(int i = 0; i<SEMMAX; i++){
+        if (p->semvec[i] == (int*)ENULL){
+            p->semvec[i] = semAdd;
+            break;
+        }
+    }
+}
+void removeBlockHelper(semd_t* walker){
+    // if it is empty, delete it and add it to free list
+    // we have 4 case in here
+    // if walker has both prev and next
+    if (walker->s_prev != (semd_t *) ENULL && walker->s_next != (semd_t *) ENULL){
+        walker->s_prev->s_next = walker->s_next;
+        walker->s_next->s_prev = walker->s_prev;
+    }// if walker has prev but not next
+    else if (walker->s_prev != (semd_t *) ENULL && walker->s_next == (semd_t *) ENULL){
+        walker->s_prev->s_next = (semd_t *) ENULL;
+    }// if walker has next but not prev which means it is head(cur_sem need to point ot next)
+    else if (walker->s_prev == (semd_t *) ENULL && walker->s_next != (semd_t *) ENULL){
+        walker->s_next->s_prev = (semd_t *) ENULL;
+        cur_sem = walker->s_next;
+    }// if walker does not have next and prev
+    else{
+        cur_sem = (semd_t *) ENULL;
+    }
+    if (free_head == (semd_t *) ENULL){
+        walker->s_prev = (semd_t *) ENULL;
+        walker->s_next = (semd_t *) ENULL;
+        walker->s_semAdd = (int*) ENULL;
+        free_head = walker;
+    }else{
+        semd_t* walker2 = free_head;
+        // insert to end of walker2
+        while(walker2->s_next!= (semd_t*)ENULL){
+            walker2 = walker2->s_next;
+        }
+        walker2->s_next = walker;
+        walker->s_prev = walker2;
+        walker->s_next = (semd_t *) ENULL;
+        walker->s_semAdd = (int*) ENULL;
+    }
+}
+int insertBlocked(int *semAdd, proc_t *p){
+    if (cur_sem == (semd_t *)ENULL){
+        cur_sem = free_head;
+        free_head = free_head->s_next;
+        cur_sem->s_next =  (semd_t *)ENULL;
+        free_head->s_prev =  (semd_t *)ENULL;
+        cur_sem->s_semAdd = semAdd;
+        // todo insert before loop or after is also ok
+        insertProc(&(cur_sem->s_link),p);
+        setSem(semAdd,p);
+        return FALSE;
+    }
+    else{
+        semd_t* prev = (semd_t *)ENULL;
+        semd_t* walker = cur_sem;
+        while(walker != (semd_t *)ENULL && walker->s_semAdd != semAdd ){
+             prev = walker;
+             walker = walker->s_next;
+        }
+        // if not found we need to create one
+        if (walker == (semd_t *)ENULL && free_head == (semd_t *)ENULL){
+                return TRUE;
+        }
+        if (walker == (semd_t *)ENULL){
+            semd_t * new_sem = free_head;
+            free_head = free_head->s_next;
+            new_sem ->s_next = (semd_t *)ENULL;
+            new_sem->s_semAdd = semAdd;
+            prev->s_next = new_sem;
+            new_sem->s_prev = prev;
+            if (free_head != (semd_t *)ENULL){
+                free_head->s_prev = (semd_t *)ENULL;
+            }
+            setSem(semAdd,p);
+            insertProc(&(new_sem->s_link),p);
+            return FALSE;
+        }else{
+            setSem(semAdd,p);
+            insertProc(& walker->s_link, p);
+            return FALSE;
+        }
+
+    }
+}
+
+int initSemd(){
+    for(int i = 0; i < MAXPROC; i++){
+        semd_table[i].s_next = (semd_t *) ENULL;
+        semd_table[i].s_prev = (semd_t *) ENULL;
+        semd_table[i].s_link.next = (proc_t*)ENULL;
+        semd_table[i].s_link.index = ENULL;
+        semd_table[i].s_semAdd = (int*)ENULL;
+    }
+    for(int i = 0; i<MAXPROC; i++){
+        if (i == 0){
+            semd_table[i].s_next = &semd_table[i+1];
+        }
+        else if ( i == MAXPROC - 1){
+            semd_table[i].s_prev = &semd_table[i-1];
+        }else{
+            semd_table[i].s_next = &semd_table[i+1];
+            semd_table[i].s_prev = &semd_table[i-1];
+        }
+    }
+    cur_sem = (semd_t *) ENULL;
+    free_head = &(semd_table[0]);
+    return 0;
+}
+proc_t* removeBlocked(int *semAdd){
+
+    semd_t* walker = cur_sem;
+    while(walker != (semd_t *) ENULL && walker->s_semAdd != semAdd  ){
+        walker = walker->s_next;
+    }
+    if (walker == (semd_t *) ENULL){
+        return (proc_t *)ENULL;
+    }
+    proc_t* rv = removeProc(&(walker->s_link));
+    setSem(semAdd,rv);
+    // to check if it empty the queue
+    proc_t* associate_queue = headBlocked(semAdd);
+    if (associate_queue == (proc_t*) ENULL){
+        removeBlockHelper(walker);
+    }
+    return rv;
+}
+
+proc_t* outBlocked(proc_t *p){
+    proc_t* rv = (proc_t*)ENULL;
+    for (int i=0;i<SEMMAX;i++){
+        if (p->semvec[i] != (int*) ENULL){
+            semd_t* walker = cur_sem;
+            while(walker != (semd_t *) ENULL && walker->s_semAdd != p->semvec[i]  ){
+                walker = walker->s_next;
+            }
+            if (walker == (semd_t *) ENULL ){
+                return  (proc_t*) ENULL;
+            }else{
+                p->semvec[i] = (int *)ENULL;
+                rv = outProc(&(walker->s_link), p);
+                proc_t* associate_queue = headBlocked(walker->s_semAdd);
+                if (associate_queue == (proc_t *) ENULL){
+                    removeBlockHelper(walker);
+                }
+            }
+        }
+    }
+    return rv;
+}
+proc_t* headBlocked(int *semAdd){
+    if (cur_sem == (semd_t *) ENULL){
+        return (proc_t *) ENULL;
+    }else{
+        semd_t* walker = cur_sem;
+        while(walker != (semd_t *) ENULL && walker->s_semAdd != semAdd  ){
+                walker = walker->s_next;
+        }
+        if (walker != (semd_t *) ENULL){
+            return headQueue(walker->s_link);
+        }
+        return (proc_t *)ENULL;
+    }
+}
+int headASL(){
+    if (cur_sem == (semd_t*) ENULL){
+        return FALSE;
+    }
+    return TRUE;
+}
---
+++ queues/procq.c
@@ -0,0 +1,196 @@
+
+#include "../h/const.h"
+#include "../h/types.h"
+#include "../h/procq.h"
+
+proc_t proc_table[MAXPROC];
+proc_t * free_head;
+char pncbuf[512];
+
+void panic(sp)
+    register char *sp;
+{
+        register char *ep = pncbuf;
+        while ((*ep++ = *sp++) != '\0');
+        asm("	trap	#0");
+}
+
+proc_t* headQueue(proc_link tp){
+    if (tp.next == (proc_t*) ENULL){
+        return (proc_t*) ENULL;
+    }
+    return tp.next->p_link[tp.index].next;
+}
+proc_t* allocProc(){
+    if (free_head == (proc_t *)ENULL) {
+        return (proc_t *)ENULL;
+    }
+    proc_t* rv  = free_head;
+    free_head = free_head->p_link[0].next;
+    rv->p_link[0].index = ENULL;
+    rv->p_link[0].next = (proc_t *)ENULL;
+    return rv;
+}
+int initProc(){
+    for (int i = 0; i< MAXPROC; i++){
+        for (int j = 0; j< SEMMAX; j++){
+            proc_table[i].p_link[j].index =  ENULL;
+            proc_table[i].p_link[j].next = (proc_t *) ENULL;
+            proc_table[i].semvec[j] = (int *)ENULL;
+        }
+        proc_table[i].qcount = 0;
+    }
+    for(int i = 0;i < MAXPROC -1; i++){
+        proc_table[i].p_link[0].index = 0;
+        proc_table[i].p_link[0].next = &(proc_table[i+1]);
+    }
+    proc_table[MAXPROC - 1].p_link[0].index = ENULL;
+    proc_table[MAXPROC - 1].p_link[0].next = (proc_t *) ENULL;
+    free_head = &(proc_table[0]);
+    return 0;
+}
+int insertProc(proc_link* tp, proc_t* p){
+    proc_t* tail = tp->next;
+    int tail_index = tp->index;
+    int i;
+    for (i = 0; i< SEMMAX; i++){
+        if (p->p_link[i].index == ENULL){
+            break;
+        }
+    }
+    if ( i == SEMMAX){
+        panic("This process is already in 20 sem queue\n");
+    }
+    // after i is found, we have 2 cases
+    // when it is first insert or not
+    if (tail == (proc_t*) ENULL){
+        // first time
+        p->p_link[i].index = i;
+        p->p_link[i].next = p;
+    }else{
+        // not first time
+        // p next point to head
+        p->p_link[i].index = tail->p_link[tail_index].index;
+        p->p_link[i].next = tail->p_link[tail_index].next;
+        // prev tail point to p
+        tail->p_link[tail_index].index = i;
+        tail->p_link[tail_index].next = p;
+    }
+    // all need to do whether it is first time or not
+    p->qcount ++;
+    tp->index = i;
+    tp->next = p;
+    return 0;
+}
+proc_t* removeProc(proc_link* tp){
+    if (tp -> next == (proc_t*) ENULL){
+        return (proc_t*) ENULL;
+    }
+    proc_t* rv;
+    // if it is only one in the queue
+    if (tp->next == headQueue(*tp)){
+        rv = tp->next;
+        rv -> p_link[tp->index].index = ENULL;
+        rv -> p_link[tp->index].next = (proc_t *)ENULL;
+        tp->index = ENULL;
+        tp->next = (proc_t *)ENULL;
+    }else{
+        // morn than 1 node in the queue
+        rv = headQueue(*tp);
+        int head_index = tp->next->p_link[tp->index].index;
+        // find the head -> next and use tp->next to point to it
+        int new_head_index = rv->p_link[head_index].index;
+        proc_t* new_head = rv->p_link[head_index].next;
+
+        tp->next->p_link[tp->index].index = new_head_index;
+        tp->next->p_link[tp->index].next = new_head;
+        rv->p_link[head_index].index = ENULL;
+        rv->p_link[head_index].next = (proc_t *)ENULL;
+    }
+    // all need to do whether it is first time or not
+    rv->qcount --;
+    return rv;
+
+}
+
+int freeProc(proc_t* p){
+    for (int i=0;i<SEMMAX;i++){
+        p->p_link[i].next = (proc_t *)ENULL;
+        p->p_link[i].index = ENULL;
+        p->semvec[i] = (int *)ENULL;
+    }
+    p->qcount = 0;
+    if (free_head == (proc_t *)ENULL){
+        free_head = p;
+    }else{
+        // insert to the tail
+        proc_t* walker = free_head;
+        while(walker->p_link[0].next != (proc_t *)ENULL){
+            walker = walker->p_link[0].next;
+        }
+        walker->p_link[0].next = p;
+    }
+    return 0;
+}
+
+proc_t* outProc(proc_link *tp, proc_t *p){
+    if (tp->next == (proc_t *)ENULL){
+        return (proc_t *)ENULL;
+    }
+    // if the head == p and there is only one node in the queue
+    proc_t* rv;
+    if (tp->next == headQueue(*tp) && tp->next == p){
+        rv = tp->next;
+        rv->p_link[tp->index].index = ENULL;
+        rv->p_link[tp->index].next = (proc_t *)ENULL;
+        tp->index = ENULL;
+        tp->next = (proc_t *)ENULL;
+        rv->qcount --;
+        return rv;
+    }
+    // if the head != p and there is only one node in the queue
+    if (tp->next == headQueue(*tp) && tp->next != p){
+        return (proc_t *)ENULL;
+    }
+    int prev_index = tp->index;
+    proc_t* pre_proc_t = tp->next;
+    int cur_index = tp->next->p_link[tp->index].index;
+    proc_t* cur_proc_t = tp->next->p_link[tp->index].next;
+    // go from head to tail (exclude tail)
+    while (cur_proc_t != tp->next){
+        // in the middle we make prev point to cur->next
+        if (cur_proc_t == p){
+           pre_proc_t->p_link[prev_index].index = cur_proc_t->p_link[cur_index].index;
+           pre_proc_t->p_link[prev_index].next = cur_proc_t->p_link[cur_index].next;
+           rv = cur_proc_t;
+           rv->p_link[cur_index].index = ENULL;
+           rv-> p_link[cur_index].next =  (proc_t *)ENULL;
+           rv -> qcount --;
+           return rv;
+        }
+        // update prev and cur
+        prev_index = cur_index;
+        pre_proc_t = cur_proc_t;
+        int copy_index = cur_index;
+        cur_index = cur_proc_t->p_link[cur_index].index;
+        cur_proc_t = cur_proc_t->p_link[copy_index].next;
+
+    }
+    // tail was found as p
+    if ( cur_proc_t == tp->next && cur_proc_t == p){
+        pre_proc_t->p_link[prev_index].index = cur_proc_t->p_link[cur_index].index;
+        pre_proc_t->p_link[prev_index].next = cur_proc_t->p_link[cur_index].next;
+        rv = cur_proc_t;
+        rv->p_link[cur_index].index = ENULL;
+        rv-> p_link[cur_index].next =  (proc_t *)ENULL;
+        rv -> qcount --;
+        tp->index = prev_index;
+        tp->next = pre_proc_t;
+        return rv;
+    }
+    // not found in any position
+    if (cur_proc_t == tp->next && cur_proc_t!= p){
+        return (proc_t *)ENULL;
+    }
+    return (proc_t *)ENULL;
+}
